be0698f7e7aa32efbcfa16ffef94e965400ad6e4
Boris Fritscher <boris@fritscher.ch>
2015-11-14 21:47:10 +0100

    pictos

--- app/scripts/controllers/boarding.js
+++ app/scripts/controllers/boarding.js
@@ -11,21 +11,24 @@ angular.module('gestionairFrontendApp')
   .controller('BoardingCtrl', function ($timeout) {
     var boarding = this;
     boarding.state = 'INIT';
+    //$0.contentDocument.getElementsByClassName('st11')[0].textContent=123
     this.board = function () {
-      boarding.state = 'INFO';
+      boarding.state = 'GUIDE1';
       //contact server
       //display info with number from server
 
     };
 
+    var timer = undefined;
     this.print = function () {
       boarding.state = 'PRINT';
       // timeout
       //TODO config on server
-      $timeout(boarding.reset, 3000);
+      timer = $timeout(boarding.reset, 10000);
     };
 
     this.reset = function () {
+      $timeout.cancel(timer);
       boarding.player = '';
       boarding.state = 'INIT';
     };
